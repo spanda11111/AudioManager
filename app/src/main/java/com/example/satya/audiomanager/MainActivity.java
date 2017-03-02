32b26aa470bfd39b40290d2ef18eaa8e56a1a2db
satya <satyapriyapanda1993@gmail.com>
2017-03-02 13:44:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/satya/audiomanager/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.satya.audiomanager;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button mode,ring,vibrate,silent;
+    private AudioManager myAudioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vibrate=(Button)findViewById(R.id.button3);
+        ring=(Button)findViewById(R.id.button2);
+
+        mode=(Button)findViewById(R.id.button);
+        silent=(Button)findViewById(R.id.button4);
+        myAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        vibrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Toast.makeText(MainActivity.this,"Now in Vibrate Mode",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                Toast.makeText(MainActivity.this,"Now in Ringing Mode",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        silent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                Toast.makeText(MainActivity.this,"Now in silent Mode",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int mod=myAudioManager.getRingerMode();
+                if(mod==AudioManager.RINGER_MODE_VIBRATE){
+                    Toast.makeText(MainActivity.this,"Now in Vibrate Mode",
+                            Toast.LENGTH_LONG).show();
+                } else if(mod==AudioManager.RINGER_MODE_NORMAL){
+                    Toast.makeText(MainActivity.this,"Now in Ringing Mode",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this,"Now in Vibrate Mode",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
